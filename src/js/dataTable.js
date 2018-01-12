1f0c5d3771a6716807c27b6bae9a4f7e9a6ff03c
tayurus <tayurus@gmail.com>
2018-01-12 11:49:57 +0300

    fix js

--- src/js/dataTable.js
+++ src/js/dataTable.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     $(".email-table__howManyShow").change(function(){
-        $("[name='pagination-page']").val($(this).text());
+        $("[name='pagination-page']").val($(".pagination__number.active").text());
         $("[type=submit]").click();
     })
 
     $(".pagination__number").click(function(){
         $("[name='pagination-page']").val($(this).text());
+        $(this).addClass("active");
         $("[type=submit]").click();
     })
 });
